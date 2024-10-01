8e703f3a5e3e5badbf9577a148a75450646092e7
kevinarlo <kevinarlo29@gmail.com>
2024-10-01 16:16:51 +0700

    edit index

--- index.php
+++ index.php
@@ -1,45 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
-<<<<<<< HEAD
-    <head>
+
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Document</title>
-    </head>
 
-    <body>
-        <h1 class='text-sm'>Hello Dinamika US</h1>
-    </body>
-    =======
+    <link rel="preconnect" href="https://www.google.com" />
+    <link rel="preconnect" href="https://www.gstatic.com" crossorigin />
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Google Recaptcha V3 -->
+    <script src="https://www.google.com/recaptcha/api.js?render=6Lco2AAjAAAAADY72bwy6ijVK9JYWkr_c6TmfRGC"></script>
 
-        <title>Document</title>
+    <!-- CSS files -->
+    <link href="assets/css/style.css" rel="stylesheet" />
 
-        <link rel="preconnect" href="https://www.google.com" />
-        <link rel="preconnect" href="https://www.gstatic.com" crossorigin />
+    <!-- Tailwind -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/4.0.0-alpha.24/lib.min.js" integrity="sha512-z/dvZbFZhHCi5/4UoyNwhidEOFbiN93Um+DWoaMZsH6zqifGGo0zA4bUwdf1SgV7KIPMQgiGs+05PNl8ytxweQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</head>
 
-        <!-- Google Recaptcha V3 -->
-        <script src="https://www.google.com/recaptcha/api.js?render=6Lco2AAjAAAAADY72bwy6ijVK9JYWkr_c6TmfRGC"></script>
+<body>
+    <h1 class='text-sm'>Hello Dinamika US</h1>
 
-        <!-- CSS files -->
-        <link href="assets/css/style.css" rel="stylesheet" />
+    <!-- JQuery -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-        <!-- Tailwind -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/4.0.0-alpha.24/lib.min.js" integrity="sha512-z/dvZbFZhHCi5/4UoyNwhidEOFbiN93Um+DWoaMZsH6zqifGGo0zA4bUwdf1SgV7KIPMQgiGs+05PNl8ytxweQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    </head>
-
-    <body>
-
-        <!-- JQuery -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
-        <!-- Main JS -->
-        <script src="assets/js/main.js"></script>
-    </body>
-
-    >>>>>>> 8bd2297d9986ab3888a0661cbd9de2a642e9a8ce
+    <!-- Main JS -->
+    <script src="assets/js/main.js"></script>
+</body>
 
 </html>
