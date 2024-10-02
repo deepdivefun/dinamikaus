730cd929c79ed8077d973659b21b8e24525a0b9d
Faisal <faisalslh97@gmail.com>
2024-10-02 15:18:23 +0700

    update header

--- includes/component/Header.php
+++ includes/component/Header.php
@@ -1,5 +1,20 @@
-<div>
-    <ul>
-        <li>Header</li>
-    </ul>
+<div class='min-h-[94px] border-b-2' >
+    <div class='mx-6'>
+        <div class='flex place-content-between my-[36px]'>
+            <div>LOGO</div>
+            <div>search</div>
+            <div>Toko</div>
+            <div>Login dan Keranjang</div>
+        </div>
+        <ul class='flex place-content-between text-normal font-normal mb-[36px]'>
+            <li class='cursor-pointer'><span class='hover:underline underline-offset-1' >Mac</span></li>
+            <li>iPad</li>
+            <li>iPhone</li>
+            <li>Watch</li>
+            <li>Music</li>
+            <li>Aksesoris</li>
+            <li>Layanan</li>
+            <li>Event Promo</li>
+        </ul>
+    </div>
 </div>
---
+++ includes/component/Hero.php
@@ -0,0 +1,3 @@
+<div class='min-h-[406px]'>
+    <h1>Hero</h1>
+</div>
