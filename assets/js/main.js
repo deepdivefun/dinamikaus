b3a48af280d12d8b1535225be41518feae7a1e67
Faisal <faisalslh97@gmail.com>
2024-11-10 19:01:10 +0700

    slider smooth scroll resposive image footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,8 +7,7 @@ function mouseOut() {
   document.getElementById("dropdown-content").style.display = "none";
 }
 
-// Initialize AOS
 AOS.init({
-  duration: 1000, // Duration of the animation (in ms)
-  // once: true, // Trigger animation only once
+  duration: 1000,
+  // once: true,
 });
