92c780e44912f83eedce4afc2a0f7ac4b5f5a088
kevinarlo <kevinarlo29@gmail.com>
2024-11-11 14:34:14 +0700

    Create class payment logo, shipping logo & changes database

--- database/dinamika.sql
+++ database/dinamika.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 11, 2024 at 08:16 AM
+-- Generation Time: Nov 11, 2024 at 08:33 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
