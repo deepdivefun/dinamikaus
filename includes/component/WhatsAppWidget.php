ab771bcadc1e35063ec43aa4f42b4114a8c4d750
kevinarlo <kevinarlo29@gmail.com>
2024-11-28 20:32:41 +0700

    Update minor

--- includes/component/WhatsAppWidget.php
+++ includes/component/WhatsAppWidget.php
@@ -1,6 +1,6 @@
 <div class="fixed bottom-5 left-5 z-50">
     <a
-        href="https://wa.me/6287888798183?text=Halo,%20saya%20tertarik%20dengan%20produk%20Anda!"
+        href="https://wa.me/6281389055595?text=Halo,%20saya%20tertarik%20dengan%20produk%20Anda!"
         target="_blank"
         rel="noopener noreferrer"
         class="flex items-center bg-green-500 text-white px-4 py-2 rounded-full shadow-lg hover:bg-green-600 transition">
